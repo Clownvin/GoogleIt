478f56ff960352da721805c5c825c2dc18aa357f
Clownvin <123chall@gmail.com>
2016-07-31 20:49:49 -0500

    Found a way to remove the 'Copy to clipboard' step, for linux systems at least

--- src/com/github/clownvin/googleit/GoogleIt.java
+++ src/com/github/clownvin/googleit/GoogleIt.java
@@ -2,26 +2,29 @@ package com.github.clownvin.googleit;
 
 import java.awt.Desktop;
 import java.awt.HeadlessException;
-import java.awt.Toolkit;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public final class GoogleIt {
 
-	public static final void main(String[] args) {
+	public static final void main(String[] args) throws IOException, InterruptedException {
+		Process xselProcess = Runtime.getRuntime().exec("xsel -o");
+		xselProcess.waitFor();
+		Scanner scanner = new Scanner(xselProcess.getInputStream());
+		scanner.useDelimiter(""+(char)-1);
 		URL searchURL = null;
 		try {
-			searchURL = new URL("https://www.google.com/#q=" + Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor).toString().trim().replace(" ", "+"));
+			searchURL = new URL("https://www.google.com/#q=" + scanner.next().trim().replace(" ", "+"));
 			openWebpage(searchURL.toURI());
-		} catch (HeadlessException | UnsupportedFlavorException | IOException | URISyntaxException e) {
+		} catch (HeadlessException | IOException | URISyntaxException e) {
 			JOptionPane.showMessageDialog(null, "The following exception has occured:\n\n "+e.getClass().getSimpleName(), "Exception Occured", JOptionPane.ERROR_MESSAGE);
 		}
+		scanner.close();
 	}
 
 	public static final void openWebpage(URI uri) {
